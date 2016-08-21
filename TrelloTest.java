1493470e48b6314b52f383fea1fe5fc77d529ea4
ER <elvict@gmail.com>
2016-08-21 11:14:41 +0300

    Added Befor and after methods

--- TrelloTest.java
+++ TrelloTest.java
@@ -4,20 +4,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class TrelloTest{
 //    private String baseUrl;
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        //        baseUrl = "https://trello.com/login/";
+        driver = new FirefoxDriver();
+//    WebDriver driver = new InternetExplorerDriver();
+        driver.get("https://trello.com/login/");
+        driver.manage().timeouts().implicitlyWait(101, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
     @Test
     public void dragAndDrop() throws Exception {
-//        baseUrl = "https://trello.com/login/";
-    WebDriver driver = new FirefoxDriver();
-//    WebDriver driver = new InternetExplorerDriver();
-    driver.get("https://trello.com/login/");
-    driver.manage().timeouts().implicitlyWait(101, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
-
     driver.findElement(By.id("user")).sendKeys("elenaelena@mail.ru");
     driver.findElement(By.id("password")).sendKeys("elenaelena");
     driver.findElement(By.id("login")).click();
@@ -29,8 +36,12 @@ public class TrelloTest{
     source = driver.findElement(By.xpath(".//a[text()=\"Second card\" and @class=\"list-card-title js-card-name\"]"));
     target = driver.findElement(By.xpath("//textarea[contains(text(),'Basics')]"));
     actions.dragAndDrop(source,target).perform();
-    driver.findElement(By.cssSelector(".member-initials")).click();
-    driver.findElement(By.linkText("Log Out")).click();
-    driver.quit();
    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.findElement(By.cssSelector(".member-initials")).click();
+        driver.findElement(By.linkText("Log Out")).click();
+        driver.quit();
+    }
 };
